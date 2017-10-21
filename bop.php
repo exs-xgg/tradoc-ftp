5f5725b67c7c93cab8ada699e97db0d24d063b25
XGG <zapperhatz26@gmail.com>
2017-10-21 18:06:08 +0800

    testing of ftp 2

---
+++ bop.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>asd</title>
+</head>
+<body>
+<form action="testftp.php" method="post" enctype="multipart/form-data">
+<input type="file" name="filex" id="filex"><br>
+<input type="submit" name="submit" value="submit">
+
+</form>
+</body>
+</html>
--- testftp.php
+++ testftp.php
@@ -1,6 +1,6 @@
 <?php 
 // set up basic connection 
-$ftp_server = "192.168.0.14"; 
+$ftp_server = "192.168.0.18"; 
 $conn_id = ftp_connect($ftp_server) or die("Could not connect to $ftp_server"); 
 
 // login with username and password 
@@ -8,8 +8,26 @@ $ftp_user_name = "tradoc";
 $ftp_user_pass = "tradocpassword"; 
 $login_result = ftp_login($conn_id, $ftp_user_name, $ftp_user_pass); 
 ftp_pasv($conn_id, true); 
-$dirs =  ftp_nlist($conn_id , "/" );
-echo $dirs;
+$dirs =  ftp_nlist($conn_id , "/home/tradoc" );
+print_r( $dirs);
+$dd=explode("\n", `cd /home/tradoc; ls`);
+foreach($dd as &$v ){
+echo '<br>'. $v;
+}
+echo '<pre>'; print_r($dd);echo '</pre>';
+echo json_encode($dd);
+
+
+
+$remote_file = "/var/www/html/tradoc-ftp/login.php";
+if (ftp_put($conn_id, $remote_file, $_FILES['filex']['tmp_name'], FTP_BINARY)) {
+ echo "successfully uploaded $file\n";
+} else {
+ echo "There was a problem while uploading $file\n";
+}
+
+
+
 //check connection 
 
 // 	if (ftp_get($conn_id, "./test.file", "index.php", FTP_BINARY)) {
