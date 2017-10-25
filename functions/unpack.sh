1179a0ca62838c0f25468599a9e57891934ef7ce
XGG <zapperhatz26@gmail.com>
2017-10-26 01:12:11 +0800

    updated zstd scripts

--- functions/unpack.sh
+++ functions/unpack.sh
@@ -5,33 +5,31 @@
 # 500 - fail
 
 # trap file name
-"$1"
 
 #create temporary folder
 mkdir temp
 
 #move file to temporary folder
-mv "$2" ./temp/"$1"
+cp "$1".zst ./temp/"$1".zst
 
 #change dir to temporary folder
 cd ./temp
 
 #zstd kicks in yoooo
-zstd -q "$1" --rm
+zstd "$1" --rm -q -d
+mv "$1" ../"$2"
 
-#move file out of the temp folder
-mv "$1".zst ../"$1".zst
 
 #delete temp folcer
 cd ..
 rm -rf ./temp 
 
-if [ -e "$1".zst ] 
+if [ -e "$2".zst ] 
 then 
     echo 200
     exit 1
 else 
-	echo "$1".zst " file doesnt exist." > errorlog_`date +"%T"`.txt
+	echo "$2".zst " file doesnt exist." > errorlog_`date +"%T"`.txt
     echo  500
     exit 0
 fi 
