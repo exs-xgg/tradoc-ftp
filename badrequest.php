9722f0a13feb3aaaf9941b3854108ecbe8aff699
XGG <zapperhatz26@gmail.com>
2017-10-29 14:34:52 +0800

    updated badrequest.php

--- badrequest.php
+++ badrequest.php
@@ -9,11 +9,7 @@
 <?php
 if (isset($_REQUEST['error'])){
 //GET IP ADDRESS OF MACHINE 
-	if (isset($_SERVER['HTTP_REFERER'])) {
-		$pager = $_SERVER['HTTP_REFERER'];
-		} else{
-			$pager = " ";
-		}
+	
 
 	$err= $_REQUEST['error'];
 	$ip = getenv('HTTP_CLIENT_IP')?:
@@ -23,7 +19,7 @@ if (isset($_REQUEST['error'])){
 	getenv('HTTP_FORWARDED')?:
 	getenv('REMOTE_ADDR');
 	echo '<div align="center"><h2>'.$ip.'</h2>';
-	echo '<i>'.$err.'</i><br><p>Origin: '. $pager . '</p></div>';
+	echo '<i>'.$err.'</i></div>';
 //LOG ERROR ON APACHE SERVER LOGS
 // `cat /var/log/httpd/error_log | grep  _ERROR_ `
 //	error_log("[ _ERROR_ ]" . $_REQUEST['error'] . " on [" . $ip . "]" . gethostbyaddr($_SERVER['REMOTE_ADDR']), 1);
