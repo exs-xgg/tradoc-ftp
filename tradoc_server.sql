b49531bba8f562358eebc7a0f7c158f9a7c33b07
XGG <zapperhatz26@gmail.com>
2017-11-01 11:13:53 +0800

    updated sql file

--- tradoc_server.sql
+++ tradoc_server.sql
@@ -1,26 +1,20 @@
-
-
---- =======
 -- phpMyAdmin SQL Dump
 -- version 4.5.1
 -- http://www.phpmyadmin.net
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 20, 2017 at 08:12 AM
+-- Generation Time: Nov 01, 2017 at 04:13 AM
 -- Server version: 10.1.13-MariaDB
 -- PHP Version: 5.6.23
-DROP DATABASE IF EXISTS tradoc;
-CREATE DATABASE IF NOT EXISTS tradoc;
-use tradoc;
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+08:00";
----- >>>>>>> 157af741f78c601728c308b3754cd4c5b5e3dff5
+SET time_zone = "+00:00";
 
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `tradoc`
@@ -34,7 +28,9 @@ SET time_zone = "+08:00";
 
 CREATE TABLE `activity` (
   `ID` int(11) NOT NULL,
-  `F_ID` int(11) NOT NULL
+  `UID` int(11) NOT NULL,
+  `ACT` varchar(100) NOT NULL,
+  `TIME` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
@@ -45,16 +41,23 @@ CREATE TABLE `activity` (
 
 CREATE TABLE `file` (
   `F_ID` int(11) NOT NULL,
+  `F_TRACK_NO` varchar(30) NOT NULL,
   `F_NAME_ORIG` varchar(36) NOT NULL,
   `F_NAME_SERVER` varchar(32) NOT NULL,
   `F_UPLOAD_DATE` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `F_DESC` varchar(200) DEFAULT NULL,
   `F_UPLOADER` int(11) NOT NULL,
-  `F_TAGS` JSON DEFAULT NULL,
+  `F_TAGS` varchar(200) DEFAULT NULL,
   `FILE_X` int(11) DEFAULT '0'
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-
+--
+-- Triggers `file`
+--
+DELIMITER $$
+CREATE TRIGGER `upload_action` AFTER INSERT ON `file` FOR EACH ROW INSERT INTO ACTIVITY (F_ID) VALUES (New.F_ID)
+$$
+DELIMITER ;
 
 -- --------------------------------------------------------
 
@@ -70,6 +73,47 @@ CREATE TABLE `messages` (
   `M_RCVR` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Triggers `messages`
+--
+DELIMITER $$
+CREATE TRIGGER `msg_sent` AFTER INSERT ON `messages` FOR EACH ROW UPDATE SSTATUS SET S_STAT = 1 WHERE S_USER = New.M_RCVR
+$$
+DELIMITER ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `office`
+--
+
+CREATE TABLE `office` (
+  `OF_ID` int(11) NOT NULL,
+  `OF_NAME` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `office`
+--
+
+INSERT INTO `office` (`OF_ID`, `OF_NAME`) VALUES
+(1, 'G6'),
+(2, 'G7'),
+(3, 'PSC'),
+(4, 'R221');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pinned`
+--
+
+CREATE TABLE `pinned` (
+  `PIN_ID` int(11) NOT NULL,
+  `PIN_USER` int(11) NOT NULL,
+  `PIN_FILE` int(11) NOT NULL,
+  `PIN_NICKNAME` varchar(20) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -83,6 +127,14 @@ CREATE TABLE `role` (
   `ROLE_DESC` varchar(50) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `role`
+--
+
+INSERT INTO `role` (`ROLE_ID`, `ROLE_NAME`, `ROLE_DESC`) VALUES
+(1, 'ADMIN', 'The one assigned to CREATE the user accounts. The '),
+(2, 'STAFF', 'Can only Upload, View and Download regular files f'),
+(3, 'SUPERUSER', 'Can upload, view and download regular and confiden');
 
 -- --------------------------------------------------------
 
@@ -107,24 +159,30 @@ CREATE TABLE `users` (
   `USER_NAME` varchar(20) NOT NULL,
   `USER_FNAME` varchar(30) NOT NULL,
   `USER_LNAME` varchar(30) NOT NULL,
+  `USER_OFC` int(11) NOT NULL,
   `USER_PW` varchar(32) NOT NULL,
-  `ROLE_ID` int(11) NOT NULL
+  `ROLE_ID` int(11) NOT NULL,
+  `USER_LOCK` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE PINNED(
-PIN_ID INT AUTO_INCREMENT PRIMARY KEY,
-PIN_USER INT NOT NULL,
-PIN_FILE INT NOT NULL,
-PIN_NICKNAME VARCHAR(20),
-FOREIGN KEY (PIN_USER) REFERENCES USERS(USER_ID),
-FOREIGN KEY (PIN_FILE) REFERENCES FILE(FILE_ID)
-);
+--
+-- Dumping data for table `users`
+--
 
-CREATE TABLE OFFICE(
-OF_ID INT PRIMARY KEY AUTO_INCREMENT not null,
-OF_NAME VARCHAR(50) NOT NULL
-);
+INSERT INTO `users` (`USER_ID`, `USER_NAME`, `USER_FNAME`, `USER_LNAME`, `USER_OFC`, `USER_PW`, `ROLE_ID`, `USER_LOCK`) VALUES
+(1, 'user01', 'Antonio', 'Luna', 2, '5f4dcc3b5aa765d61d8327deb882cf99', 1, 0),
+(2, 'adminAq', 'Xander', 'Ford', 3, '5f4dcc3b5aa765d61d8327deb882cf99', 2, 0),
+(3, 'general', 'Rodrigo', 'Duterte', 1, '5f4dcc3b5aa765d61d8327deb882cf99', 3, 0),
+(4, 'turtle', 'asd', 'asd', 2, 'a3dcb4d229de6fde0db5686dee47145d', 1, 1);
 
+--
+-- Triggers `users`
+--
+DELIMITER $$
+CREATE TRIGGER `init_user` AFTER INSERT ON `users` FOR EACH ROW INSERT INTO SSTATUS (S_USER)
+  VALUES (New.USER_ID)
+$$
+DELIMITER ;
 
 --
 -- Indexes for dumped tables
@@ -134,8 +192,7 @@ OF_NAME VARCHAR(50) NOT NULL
 -- Indexes for table `activity`
 --
 ALTER TABLE `activity`
-  ADD PRIMARY KEY (`ID`),
-  ADD KEY `F_ID` (`F_ID`);
+  ADD PRIMARY KEY (`ID`);
 
 --
 -- Indexes for table `file`
@@ -151,6 +208,20 @@ ALTER TABLE `messages`
   ADD PRIMARY KEY (`M_ID`),
   ADD KEY `M_SENDER` (`M_SENDER`),
   ADD KEY `M_RCVR` (`M_RCVR`);
+
+--
+-- Indexes for table `office`
+--
+ALTER TABLE `office`
+  ADD PRIMARY KEY (`OF_ID`);
+
+--
+-- Indexes for table `pinned`
+--
+ALTER TABLE `pinned`
+  ADD PRIMARY KEY (`PIN_ID`),
+  ADD KEY `PIN_TO_FILE` (`PIN_FILE`),
+  ADD KEY `PIN_TO_USER` (`PIN_USER`);
 
 --
 -- Indexes for table `role`
@@ -180,17 +251,27 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `activity`
 --
 ALTER TABLE `activity`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 --
 -- AUTO_INCREMENT for table `file`
 --
 ALTER TABLE `file`
-  MODIFY `F_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `F_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
   MODIFY `M_ID` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `office`
+--
+ALTER TABLE `office`
+  MODIFY `OF_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- AUTO_INCREMENT for table `pinned`
+--
+ALTER TABLE `pinned`
+  MODIFY `PIN_ID` int(11) NOT NULL AUTO_INCREMENT;
 --
 -- AUTO_INCREMENT for table `role`
 --
@@ -200,21 +281,15 @@ ALTER TABLE `role`
 -- AUTO_INCREMENT for table `sstatus`
 --
 ALTER TABLE `sstatus`
-  MODIFY `S_ID` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `S_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `USER_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 --
 -- Constraints for dumped tables
 --
-
---
--- Constraints for table `activity`
---
-ALTER TABLE `activity`
-  ADD CONSTRAINT `activity_ibfk_1` FOREIGN KEY (`F_ID`) REFERENCES `file` (`F_ID`);
 
 --
 -- Constraints for table `file`
@@ -228,6 +303,13 @@ ALTER TABLE `file`
 ALTER TABLE `messages`
   ADD CONSTRAINT `messages_ibfk_1` FOREIGN KEY (`M_SENDER`) REFERENCES `users` (`USER_ID`),
   ADD CONSTRAINT `messages_ibfk_2` FOREIGN KEY (`M_RCVR`) REFERENCES `users` (`USER_ID`);
+
+--
+-- Constraints for table `pinned`
+--
+ALTER TABLE `pinned`
+  ADD CONSTRAINT `PIN_TO_FILE` FOREIGN KEY (`PIN_FILE`) REFERENCES `file` (`F_ID`),
+  ADD CONSTRAINT `PIN_TO_USER` FOREIGN KEY (`PIN_USER`) REFERENCES `users` (`USER_ID`);
 
 --
 -- Constraints for table `sstatus`
@@ -244,37 +326,3 @@ ALTER TABLE `users`
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-
---
--- Triggers `users`
---
-DELIMITER $$
-CREATE TRIGGER `init_user` AFTER INSERT ON `users` FOR EACH ROW INSERT INTO SSTATUS (S_USER)
-  VALUES (New.USER_ID)
-$$
-DELIMITER ;
---
--- Triggers `messages`
---
-DELIMITER $$
-CREATE TRIGGER `msg_sent` AFTER INSERT ON `messages` FOR EACH ROW UPDATE SSTATUS SET S_STAT = 1 WHERE S_USER = New.M_RCVR
-$$
-DELIMITER ;
---
--- Triggers `file`
---
-DELIMITER $$
-CREATE TRIGGER `upload_action` AFTER INSERT ON `file` FOR EACH ROW INSERT INTO ACTIVITY (F_ID) VALUES (New.F_ID)
-$$
-DELIMITER ;
-
-
-INSERT INTO ROLE
-VALUES (1,'ADMIN', 'The one assigned to CREATE the user accounts. The only role capable of reading server status and editing the database, with the permission of the OIC'),
-(2,'STAFF', 'Can only Upload, View and Download regular files from the server. Also capable of using the LAN Messaging feature.'),
-(3,'SUPERUSER', 'Can upload, view and download regular and confidential files. Also capable of using the LAN Messaging feature. Cannot have the privileges of the ADMIN')
-
-INSERT INTO USERS VALUES(USER_NAME, USER_FNAME, USER_LNAME, USER_PW, ROLE_ID)
-VALUES ('user01', 'Antonio', 'Luna', '5f4dcc3b5aa765d61d8327deb882cf99',1),
-('adminAq', 'Xander', 'Ford', '5f4dcc3b5aa765d61d8327deb882cf99',2),
-('general', 'Rodrigo', 'Duterte', '5f4dcc3b5aa765d61d8327deb882cf99',3);
