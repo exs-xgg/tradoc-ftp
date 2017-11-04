c4dc8a728e6f643c33a38a8de77dc6549ff4ac6c
XGG <zapperhatz26@gmail.com>
2017-11-04 18:31:20 +0800

    added missing parameter

--- functions/post_file.php
+++ functions/post_file.php
@@ -27,17 +27,17 @@ if (isset($_SESSION['user']) && isset($_POST['submit'])){
 //if (isset($_POST['submit'])){
 	$person = new User;
 	$person = unserialize($_SESSION['user']);
-	$desc = $_POST['desc'];
 	$filetrack = fin($_POST['filetrack']);
 	$desc = fin(strip_tags($_POST['desc']));
 	$tags = json_encode(explode("\r\n", (strip_tags(str_replace("'", "",$_POST['tags'])))));
 	echo $tags.'<br>';
-	$file_orig =str_replace(" ", "_",  $_FILES['filex']['name']);
+	$file_orig = str_replace("'","",str_replace(" ", "_",  $_FILES['filex']['name']));
 	$file_orig = fin(strip_tags($file_orig));
 
 
 	//CHECK FOR FILE EXTENSION
 	//THESE ARE THE ALLOWED EXTENSIONS
+	//RECOMMENDATION: PULL EXTENSIONS FROM DATABASE
 	$exts = array('docx', 'doc', 'ppt', 'pptx', 'xls', 'xlsx', 'pdf', 'txt', 'odt', 'ods', 'odp'); 
 	if(!in_array(end(explode('.', $file_orig)), $exts)){
 		$uri = strtok($_SERVER['HTTP_REFERER'],'?');
@@ -63,7 +63,7 @@ if (isset($_SESSION['user']) && isset($_POST['submit'])){
 		$uri = strtok($_SERVER['HTTP_REFERER'],'?');
 		
 		$ofc = $person->user_office;
-		$sql = 	"INSERT INTO file(F_TRACK_NO,F_NAME_ORIG,F_NAME_SERVER,F_DESCF_UPLOADER,F_OFFICE,F_TAGS,FILE_X)
+		$sql = 	"INSERT INTO file(F_TRACK_NO,F_NAME_ORIG,F_NAME_SERVER,F_DESC,F_UPLOADER,F_OFFICE,F_TAGS,FILE_X)
 		VALUES('$filetrack','$file_orig','$randomString', '$desc',$person->user_id, '$ofc','$tags',0)";
 		echo '<br><br>'.$sql;
 		if($conn->query($sql)){
