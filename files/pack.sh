2f91a26bb78b0e55a0488b36ce1c9c1de70bbca9
XGG <zapperhatz26@gmail.com>
2017-11-05 20:17:10 +0800

    patch up

--- files/pack.sh
+++ files/pack.sh
@@ -3,23 +3,23 @@
 # return status codes:
 # 200 - success
 # 500 - fail
-
-mv "$1" ./temp/"$1"
-cd ./temp
+ 
+mv "$1" /var/www/html/tradoc-ftp/files/temp/"$1"
+cd /var/www/html/tradoc-ftp/files/temp
 
 /usr/local/bin/zstd -q "$1" --rm
 
-mv "$1".zst ../"$1".zst
+mv /var/www/html/tradoc-ftp/files/temp/"$1".zst /var/www/html/tradoc-ftp/files/"$1".zst
 
 cd ..
-rm -rf ./temp/*.zst
+rm -rf /var/www/html/tradoc-ftp/files/temp/*.zst
 
-if [ -e "$1".zst ] 
+if [ -e /var/www/html/tradoc-ftp/files/"$1".zst ] 
 then 
     echo 200
     exit 1
 else 
-	echo "$1".zst " file doesnt exist." > errorlog_`date +"%T"`.txt
+	echo "$1".zst " file doesnt exist." 
     echo  500
     exit 0
 fi 
