6ed5cd5ede78ac55e37ceac613ed1ab45fe13754
XGG <zapperhatz26@gmail.com>
2017-11-16 09:50:02 +0800

    updated init process

--- init_server.sh
+++ init_server.sh
@@ -1,16 +1,21 @@
-#SCRIPT TO AUTOMATE SERVER SETUP
+#!/bin/bash
+# SCRIPT TO AUTOMATE SERVER SETUP
 
-#init packages
-sudo yum install httpd -y
-sudo yum install mysql-server -y 
-sudo yum install php-mysql php-ftp php -y
-yum groupinstall "Development tools" -y
-sudo yum install git -y
+# init packages
+# sudo yum install httpd -y
+# sudo yum install mysql-server -y 
+# sudo yum install php-mysql php-ftp php -y
+# yum groupinstall "Development tools" -y
+# sudo yum install git -y
 
 
-mkdir files
 chown apache:apache /var/www/html
-chmod 775 /var/www/html/tradoc-ftp
-mysql -u root -p < tradoc_server.sql
+chown apache:apache /var/www/html/tradoc-ftp
+chmod 777 /var/www/html/tradoc-ftp
+chown apache:apache /var/www/html/tradoc-ftp/files
+chown apache:apache /var/www/html/tradoc-ftp/files/temp
+chmod 777 /var/www/html/tradoc-ftp/files
+chmod 777 /var/www/html/tradoc-ftp/files/temp
+chmod 777 /var/www/html/tradoc-ftp/files/*.sh
 
 echo "======DONE SETUP======="
