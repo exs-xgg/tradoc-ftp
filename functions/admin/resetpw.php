1983aa5855baa6ee3267803a2620d18632b8195e
XGG <zapperhatz26@gmail.com>
2017-11-24 21:59:21 +0800

    mods

--- functions/admin/resetpw.php
+++ functions/admin/resetpw.php
@@ -16,7 +16,7 @@ if (isset($_SESSION['user'])) {
 		$uid = $person->user_id;
 		$uid = $_REQUEST['uid'];
 		//QUERY TO DATABASE IF PIN
-		$sql = 	"UPDATE users set USER_PW=md5('') where USER_ID=$uid";
+		$sql = 	"UPDATE users set USER_PW=md5('password') where USER_ID=$uid";
 
 		if($conn->query($sql)){
 x_log("modify",  $_SERVER['REQUEST_URI'] ,$person->user_id);
